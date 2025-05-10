c5d28f1e9db644d51da6575533c45164ba1acccc
Connection <69660530+ConnectionOuOb@users.noreply.github.com>
2025-05-10 21:46:50 +0800

    feat: Show prompt

--- lib/dialog.dart
+++ lib/dialog.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/cupertino.dart';
+
 import 'tts.dart';
 import 'object.dart';
 import 'package:flutter/material.dart';
@@ -38,7 +40,26 @@ Widget popUpImage(BuildContext context, PopImage popImage) {
                       width: 20.w,
                       margin: const EdgeInsets.only(left: 35),
                       alignment: Alignment.center,
-                      child: Text(popImage.imageText, style: const TextStyle(fontSize: 20)),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(popImage.imageText, style: const TextStyle(fontSize: 22)),
+                          const SizedBox(height: 10),
+                          const Divider(),
+                          const SizedBox(height: 10),
+                          const Text(
+                            "關鍵提示詞:",
+                            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: 5),
+                          ...popImage.prompts.asMap().entries.map(
+                                (e) => Text(
+                                  "${e.key + 1}. ${e.value}",
+                                  style: TextStyle(fontSize: 16, color: Colors.grey[800]),
+                                ),
+                              ),
+                        ],
+                      ),
                     ),
                   ),
               ],
